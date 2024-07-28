7762c7164ab6912d6e2d6711173ece059eb652a6
DESKTOP-T8SP4NE\Adrian Pereira <a.pereira.2@estudiantec.cr>
2024-07-28 12:31:54 -0600

    envia

--- SERVER.cs
+++ SERVER.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using
+using System.Net;
+using System.Net.Sockets;
 
 namespace WHAT
 {
-    internal class SERVER
+    class SERVER
+
     {
+        IPHostEntry host;
+        IPAddress ipAddr;
+        IPEndPoint endpoint;
+        Socket s_Server;
+        Socket s_cliente;
+
+        public SERVER(string ip, int port)
+        {
+            host = Dns.GetHostByName(ip);
+            ipAddr = host.AddressList[0];
+            endpoint = new IPEndPoint(ipAddr, port);
+
+            s_Server = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            s_Server.Bind(endpoint);
+            s_Server.Listen(10);
+
+        }
+        public void Start()
+        {
+            byte[] buffer = new byte[1024];
+            string message;
+
+            s_cliente = s_Server.Accept();
+            s_cliente.Receive(buffer);
+            message = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine("Se recibio el mensaje:" + message);
+        }
     }
 }
